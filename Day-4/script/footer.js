ee8c0ee5eb7c4644afa5892473f3c0b263102a5e
bhairavgotam <bhairavgotam08@gmail.com>
2022-10-02 18:22:13 +0530

    added day-5 files

---
+++ Day-4/script/footer.js
@@ -0,0 +1,12 @@
+function Subscribe()
+{
+    if(document.querySelector("#SubscribeEmail").value !="")
+    {
+        alert("Thanks for subscribing!");
+        
+        document.querySelector("#SubscribeEmail").value="";
+    }
+    
+}
+
+export {Subscribe};
---
+++ Day-4/script/index.js
@@ -0,0 +1,25 @@
+import {navbar, footer }from "../components/navbarAndFooterExp.js";
+document.querySelector("#navbar").innerHTML = navbar();
+
+document.querySelector("#footer").innerHTML = footer();
+
+
+document.querySelector("#startFreeTri").addEventListener("click", startFreeTri);
+function startFreeTri()
+{
+    if (document.querySelector("#workEmail").value != "")
+    {
+        document.querySelector("#workEmail").value="";
+        alert("Your Free Trial has start!");
+    }
+}
+
+document.querySelector("#Ipart9Submit").addEventListener("click", freeTrial);
+function freeTrial()
+{
+    if (document.querySelector("#Ipart9Email").value != "")
+    {
+        document.querySelector("#Ipart9Email").value="";
+        alert("Your Free Trial has start!");
+    }
+}
